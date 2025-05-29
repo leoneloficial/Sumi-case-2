424fb0852dc608a84e5cefb630beb16398b18751
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2025-05-29 01:07:45 -0300

    Update gc-discord.js

--- plugins/gc-discord.js
+++ plugins/gc-discord.js
@@ -4,7 +4,6 @@ handler.help = ['discord']
 handler.tags = ['grupo']
 handler.command = /^(serverdis|serverdiscord|discord|grupodiscord|linkdiscord)$/i
 handler.group = true;
-export default handler
 
 global.discord = `Nuestro Server de Discord!
 Link: https://discord.gg/UjdSaTESQG`
