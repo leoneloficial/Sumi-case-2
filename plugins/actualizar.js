345f82f6901e1fa229898499c32e60e94955425c
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2025-05-29 02:36:51 -0300

    Update actualizar.js

--- plugins/actualizar.js
+++ plugins/actualizar.js
@@ -10,29 +10,24 @@ const handler = async (msg, { conn, text }) => {
   });
 
   try {
-    const { db } = global;
-    const idioma = db?.data?.users?.[msg.sender]?.language || global.defaultLenguaje;
-    const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`));
-    const tradutor = _translate.plugins.propietario_actualizar;
+    const stdout = execSync('git pull' + (msg.fromMe && text ? ' ' + text : ''));
+    let messager = stdout.toString();
 
+    if (messager.includes('Already up to date.')) {
+      messager = '✅ Ya está todo actualizado.';
+    } else if (messager.includes('Updating')) {
+      messager = '✅ Se han aplicado actualizaciones:\n\n' + stdout.toString();
+    }
+
+    await conn.sendMessage(chatId, { text: messager }, { quoted: msg });
+
+    // Reacción de éxito
+    await conn.sendMessage(chatId, {
+      react: { text: '✅', key: msg.key }
+    });
+
+  } catch {
     try {
-      const stdout = execSync('git pull' + (msg.fromMe && text ? ' ' + text : ''));
-      let messager = stdout.toString();
-
-      if (messager.includes('Already up to date.')) {
-        messager = tradutor.texto1;
-      } else if (messager.includes('Updating')) {
-        messager = tradutor.texto2 + '\n\n' + stdout.toString();
-      }
-
-      await conn.sendMessage(chatId, { text: messager }, { quoted: msg });
-
-      // Reacción de éxito
-      await conn.sendMessage(chatId, {
-        react: { text: '✅', key: msg.key }
-      });
-
-    } catch {
       const status = execSync('git status --porcelain');
       if (status.length > 0) {
         const conflictedFiles = status
@@ -50,18 +45,18 @@ const handler = async (msg, { conn, text }) => {
           .filter(Boolean);
 
         if (conflictedFiles.length > 0) {
-          const errorMessage = `${tradutor.texto3}\n\n${conflictedFiles.join('\n')}.*`;
+          const errorMessage = `⚠️ Archivos modificados que podrían causar conflictos:\n\n${conflictedFiles.join('\n')}.*`;
           await conn.sendMessage(chatId, { text: errorMessage }, { quoted: msg });
         }
       }
+    } catch (error) {
+      console.error(error);
+      let errorMessage2 = '❌ Error al verificar conflictos en git.';
+      if (error.message) {
+        errorMessage2 += '\n*- Mensaje de error:* ' + error.message;
+      }
+      await conn.sendMessage(chatId, { text: errorMessage2 }, { quoted: msg });
     }
-  } catch (error) {
-    console.error(error);
-    let errorMessage2 = '❌ Ocurrió un error al intentar traducir o acceder a la base de datos.';
-    if (error.message) {
-      errorMessage2 += '\n*- Mensaje de error:* ' + error.message;
-    }
-    await conn.sendMessage(chatId, { text: errorMessage2 }, { quoted: msg });
   }
 };
 
