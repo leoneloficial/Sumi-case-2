4b9e92acfb2d00b2655fc35fce7ae6602265c174
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-06-07 14:10:41 -0400

    Actualizar Bots.js

--- plugins/Bots.js
+++ plugins/Bots.js
@@ -35,14 +35,14 @@ const handler = async (msg, { conn }) => {
     mentions.push(fullJid);
     const prefijo = dataPrefijos[fullJid] || ".";
 
-    return `> *❀Subbot ${i + 1}*\n> ❀Número: @${jid}\n> ✦ Prefijo: *${prefijo}*\n───────────────`;
+    return `> *❀Subbot ${i + 1}*\n> *❀Número*: @${jid}\n> *✦ Prefijo*: *${prefijo}*\n───────────────`;
   }).join("\n\n");
 
   // Construir mensaje final
   const menu = `\n*「✦」subbots activos*
 
 ❀ para ser un subbot usa el comando *#code*
- *❀Subbots Conectados* *${total}*\n\n\n${lista}`;
+ *❀Subbots Conectados:* *${total}*\n\n\n${lista}`;
 
   // Enviar usando sendMessage2
   await conn.sendMessage2(
