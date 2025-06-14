74df3ac8c6eda30f237b0b0e0b55473e87a72693
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-06-13 21:23:30 -0400

    Actualizar Antis.js

--- plugins/Antis.js
+++ plugins/Antis.js
@@ -27,10 +27,8 @@ const handler = async (msg, { conn, args }) => {
 
   if (!args[0] || !["on", "off"].includes(args[0].toLowerCase())) {
     return conn.sendMessage(chatId, {
-      text: "*「✦」Un administrador puede activar o desactivar este comando utilizando
-
-*✎ antis on* 
-*✎ antis off*"
+      text: "*「✦」Un administrador puede activar o desactivar este comando utilizando/n
+leonel"
     }, { quoted: msg });
   }
 
